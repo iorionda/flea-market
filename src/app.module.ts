82c242f1e0cd2985cfafa583839b3d4273054695
Iori ONDA <iorionda@gmail.com>
2023-05-09 01:37:34 +0900

    :tada: Initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [],
+  imports: [ItemsModule],
   controllers: [],
   providers: [],
+  // exports: [],外部モジュールにエクスポートとしたい時に利用する
 })
 export class AppModule {}
